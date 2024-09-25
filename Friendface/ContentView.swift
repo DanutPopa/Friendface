100b150e49b818169f92e940c67dd937cd8872b4
DanutPopa <popa.ion.danut@gmail.com>
2024-09-25 19:26:10 +0300

    Fetching and displaying the data from a url.

--- Friendface/ContentView.swift
+++ Friendface/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+   @State private var users = [User]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(users, id: \.id) { user in
+                Text(user.name)
+            }
+            .navigationTitle("Friendface")
+            .toolbar {
+                Button("Show Friends") {
+                    Task {
+                        await fetchFriends()
+                    }
+                }
+            }
         }
-        .padding()
+    }
+    
+    func fetchFriends() async {
+        
+        guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else { return }
+        
+        do {
+            if users.isEmpty {
+                let (data, _) = try await URLSession.shared.data(from: url)
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                if let decoded = try? decoder.decode([User].self, from: data) {
+                    users = decoded
+                }
+            }
+        } catch {
+            print("There was an error downloading the data: \(error.localizedDescription)")
+        }
     }
 }
 
