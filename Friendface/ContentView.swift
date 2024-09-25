3ac7ac5e6c88bc356167c770821f2e3697a49b1c
DanutPopa <popa.ion.danut@gmail.com>
2024-09-25 14:53:19 +0300

    Initial Commit

---
+++ Friendface/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Friendface
+//
+//  Created by Danut Popa on 25.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
